bf82e75cb693b7dd54dfe3c26918813eb54621d2
NikitaMiryushchenko <miri@sfedu.ru>
2023-08-31 17:33:14 +0300

    some correct

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,18 +3,22 @@ import { useState } from "react";
 const Hello = () => {
   const [point, setPoint] = useState(0);
 
-    const incrFunc = () => {
-        setPoint(point + 1)
-    }
-    const decrFunc = () => {
-        setPoint(point - 1)
-    }
+  const incrFunc = () => {
+    setPoint((prev) => prev + 1);
+  };
+  const decrFunc = () => {
+    setPoint((prev) => prev - 4);
+  };
 
   return (
     <div>
       <h1>React</h1>
-      <button point={point} onClick={incrFunc}>+1</button>
-      <button point={point} onClick={decrFunc}>-1</button>
+      <button point={point} onClick={incrFunc}>
+        +1
+      </button>
+      <button point={point} onClick={decrFunc}>
+        -1
+      </button>
       <p>{point}</p>
     </div>
   );
